bc9d5bc77d4a3160385fd5204658ea03c0994001
hejiawencc <hjw0415@outlook.com>
2026-04-20 13:57:32 +0800

    rootfs 添加usbmount软件包

--- mk-ubuntu-rootfs.sh
+++ mk-ubuntu-rootfs.sh
@@ -201,7 +201,7 @@ export DEBIAN_FRONTEND=noninteractive
 export APT_INSTALL="apt-get install -fy --allow-downgrades"
 
 echo -e "\033[47;36m ---------- LubanCat -------- \033[0m"
-\${APT_INSTALL} dialog toilet u-boot-tools edid-decode logrotate fire-config lbc-test
+\${APT_INSTALL} dialog toilet u-boot-tools edid-decode logrotate fire-config lbc-test usbmount
 if [[ "$TARGET" == "gnome" || "$TARGET" == "gnome-full" ]]; then
     \${APT_INSTALL} gdisk fire-config-gui
     #Desktop background picture
